2773e38506a0a6c6bec57fff6303b91157f6b364
Tim Upchurch <mail@timup.ch>
2019-04-25 19:41:30 -0400

    graphql back working, much has changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,146 +2,143 @@ const path = require('path')
 const config = require('./data/config')
 
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+	path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-  siteMetadata: {
-    title: config.defaultTitle,
-    description: config.defaultDescription,
-    author: config.author,
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/static/images`,
-        name: 'uploads',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/assets/images`,
-        name: 'images',
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    // {
-    //   resolve: 'gatsby-source-graphql',
-    //   options: {
-    //     typeName: 'GitHub',
-    //     fieldName: 'github',
-    //     url: 'https://api.github.com/graphql',
-    //     headers: {
-    //       Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
-    //     },
-    //     fetchOptions: {},
-    //   },
-    // },
-    {
-      resolve: 'gatsby-plugin-nprogress',
-      options: {
-        color: config.themeColor,
-        showSpinner: false,
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              destinationDir: 'static',
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: config.googleAnalyticsID,
-        head: true,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-favicon',
-      options: {
-        logo: './static/images/black-flag.png',
-        injectHTML: true,
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          twitter: false,
-          yandex: false,
-          windows: false,
-        },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: config.defaultTitle,
-        short_name: 'starter',
-        start_url: '/',
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'minimal-ui',
-        icon: './static/images/black-flag.png',
-      },
-    },
-    {
-      resolve: `gatsby-plugin-alias-imports`,
-      options: {
-        alias: {
-          Components: path.resolve(__dirname, 'src/components'),
-          Common: path.resolve(__dirname, 'src/components/common'),
-          Static: path.resolve(__dirname, 'static/'),
-          Theme: path.resolve(__dirname, 'src/components/theme'),
-          Data: path.resolve(__dirname, 'data/config'),
-        },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-purgecss', // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
+	siteMetadata: {
+		title: config.defaultTitle,
+		description: config.defaultDescription,
+		author: config.author,
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-styled-components',
+		{
+			// keep as first gatsby-source-filesystem plugin for gatsby image support
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/static/images`,
+				name: 'uploads',
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/pages`,
+				name: 'pages',
+			},
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				path: `${__dirname}/src/assets/images`,
+				name: 'images',
+			},
+		},
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-graphql',
+			options: {
+				typeName: 'GitHub',
+				fieldName: 'github',
+				url: 'https://api.github.com/graphql',
+				headers: {
+					Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+				},
+				fetchOptions: {},
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-nprogress',
+			options: {
+				color: config.themeColor,
+				showSpinner: false,
+			},
+		},
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: [
+					{
+						resolve: 'gatsby-remark-relative-images',
+						options: {
+							name: 'uploads',
+						},
+					},
+					{
+						resolve: 'gatsby-remark-images',
+						options: {
+							maxWidth: 2048,
+						},
+					},
+					{
+						resolve: 'gatsby-remark-copy-linked-files',
+						options: {
+							destinationDir: 'static',
+						},
+					},
+				],
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				trackingId: config.googleAnalyticsID,
+				head: true,
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-favicon',
+			options: {
+				logo: './static/images/black-flag.png',
+				injectHTML: true,
+				icons: {
+					android: true,
+					appleIcon: true,
+					appleStartup: true,
+					coast: false,
+					favicons: true,
+					firefox: true,
+					twitter: false,
+					yandex: false,
+					windows: false,
+				},
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: config.defaultTitle,
+				short_name: 'starter',
+				start_url: '/',
+				background_color: config.backgroundColor,
+				theme_color: config.themeColor,
+				display: 'minimal-ui',
+				icon: './static/images/black-flag.png',
+			},
+		},
+		{
+			resolve: `gatsby-plugin-alias-imports`,
+			options: {
+				alias: {
+					Components: path.resolve(__dirname, 'src/components'),
+					Common: path.resolve(__dirname, 'src/components/common'),
+					Static: path.resolve(__dirname, 'static/'),
+					Theme: path.resolve(__dirname, 'src/components/theme'),
+					Data: path.resolve(__dirname, 'data/config'),
+				},
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-netlify-cms',
+			options: {
+				modulePath: `${__dirname}/src/cms/cms.js`,
+			},
+		},
+		'gatsby-plugin-offline',
+		'gatsby-plugin-purgecss', // must be after other CSS plugins
+		'gatsby-plugin-netlify', // make sure to keep it last in the array
+	],
 }
--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Thumbnail from 'Static/thumbnail/thumbnail.png'
+
 import {
 	url,
 	defaultDescription,
@@ -19,7 +20,7 @@ export const SEO = ({
 	description = defaultDescription,
 	location = '',
 }) => {
-	const structuredDataOrganization = `{ 
+	const organizationStructuredData = `{ 
 		"@context": "http://schema.org",
 		"@type": "Organization",
 		"legalName": "${legalName}",
@@ -51,30 +52,50 @@ export const SEO = ({
 			"${socialLinks.instagram}",
 			"${socialLinks.github}"
 		]
-  	}`
+      }`
 
 	return (
 		<Helmet>
+			<html lang="en" dir="ltr" />
+			<title>{title}</title>
+
 			<meta name="description" content={description} />
 			<meta name="image" content={Thumbnail} />
 
-			<meta property="og:url" content={`${url}${location}/?ref=smakosh.com`} />
-			<meta property="og:type" content="website" />
-			<meta property="og:title" content={title} />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/img/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				href="/img/favicon-32x32.png"
+				sizes="32x32"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				href="/img/favicon-16x16.png"
+				sizes="16x16"
+			/>
+			<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+			<meta name="theme-color" content="#fff" />
+
 			<meta property="og:description" content={description} />
 			<meta property="og:image" content={Thumbnail} />
-			<meta property="fb:app_id" content={social.facebook} />
+			<meta property="og:title" content={title} />
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={`${url}${location}/?ref=timup.ch`} />
 
 			<meta name="twitter:card" content="summary" />
 			<meta name="twitter:creator" content={socialLinks.twitter} />
-			<meta name="twitter:site" content={social.twitter} />
-			<meta name="twitter:title" content={title} />
 			<meta name="twitter:description" content={description} />
 			<meta name="twitter:image:src" content={Thumbnail} />
-			<script type="application/ld+json">{structuredDataOrganization}</script>
-			<link rel="publisher" href={socialLinks.google} />
-			<title>{title}</title>
-			<html lang="en" dir="ltr" />
+			<meta name="twitter:site" content={social.twitter} />
+			<meta name="twitter:title" content={title} />
+
+			<script type="application/ld+json">{organizationStructuredData}</script>
 		</Helmet>
 	)
 }
