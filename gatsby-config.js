f25817ebe6f718794c0452827fc5841ac6a9ec50
Tim Upchurch <mail@timup.ch>
2021-03-16 19:17:28 -0400

    fix this janky build... once and for all.

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,9 +134,6 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
     },
     'gatsby-plugin-offline',
     // 'gatsby-plugin-purgecss', // must be after other CSS plugins
